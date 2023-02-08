065279ed1b7dc15cce3da01524828bfdd622cb5f
Francis Pelletier <francispelletier2@hotmail.com>
2023-02-07 21:32:48 -0500

    Feat: Add a protected wrapper with layout to a profile route

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { TodolistSchema, TodoPayload, TodoSchema, UserSchema } from "../types";
 
 /* const base = process.env.API_ENDPOINT; */
 const base = "http://127.0.0.1:1337";
@@ -6,6 +7,12 @@ const base = "http://127.0.0.1:1337";
 const userBase = `${base}/api/users`;
 const sessionBase = `${base}/api/sessions`;
 const todolistBase = `${base}/api/todolist`;
+
+axios.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
+  "access_token"
+)}`;
+
+axios.defaults.headers["x-refresh"] = localStorage.getItem("refresh_token");
 
 export async function registerUser(payload: {
   username: string;
@@ -22,4 +29,18 @@ export async function login(payload: { email: string; password: string }) {
     localStorage.setItem("refresh_token", res.data.refreshToken);
     return res.data;
   });
+}
+
+export async function getUser(): Promise<UserSchema> {
+  return axios
+    .get(userBase)
+    .then((res) => {
+      if (res.headers["x-access-token"]) {
+        localStorage.setItem("access_token", res.headers["x-access-token"]);
+      }
+      return res.data;
+    })
+    .catch(() => {
+      return null;
+    });
 }
---
+++ src/mocks/utils.ts
@@ -0,0 +1,12 @@
+import { TodolistSchema, TodoSchema, UserSchema } from "../types";
+
+export const user: UserSchema = {
+  _id: "63c751fca1f2de672f60a517",
+  email: "userOne@test.com",
+  username: "userOne",
+  createdAt: "2023-01-18T01:57:16.458Z",
+  updatedAt: "2023-01-18T01:57:16.458Z",
+  session: "63d6bf35a1f2de672f614a30",
+  iat: 1675213057,
+  exp: 1675213957,
+};
