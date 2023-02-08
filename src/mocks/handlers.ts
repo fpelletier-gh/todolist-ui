a5e0f4023417e100d5e0cb4db69abfe253d6a1d7
Francis Pelletier <francispelletier2@hotmail.com>
2023-02-07 20:29:46 -0500

    Feat: Add a login route with tests and mocks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -11,5 +11,20 @@ export const unauthorizedUserHandler = rest.all(userBase, (req, res, ctx) => {
   return res(ctx.status(403));
 });
 
+export const unauthorizedSessionHandler = rest.all(
+  sessionBase,
+  (req, res, ctx) => {
+    return res(ctx.status(403));
+  }
+);
+
 export const handlers = [
+  rest.post(sessionBase, async (req, res, ctx) => {
+    const { email } = await req.json();
+    if (email === "validUser@test.com") {
+      return res(ctx.status(200), ctx.json(session));
+    } else {
+      return res(ctx.status(403));
+    }
+  }),
 ];
---
+++ src/routes/auth/login.tsx
@@ -0,0 +1,93 @@
+import {
+  Button,
+  Container,
+  Group,
+  Paper,
+  PasswordInput,
+  Stack,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { showNotification } from "@mantine/notifications";
+import { IconX } from "@tabler/icons";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../api";
+import { useUser } from "../../context/user";
+
+function LoginPage() {
+  const user = useUser();
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  // TODO: Refactor mutation to custom hook
+  const mutation = useMutation(login, {
+    onMutate: () => {
+      queryClient.invalidateQueries(["user"]);
+    },
+    onSuccess: () => {
+      const newUser = async () => await user.login();
+      navigate("/todolist", { state: { user: newUser, replace: true } });
+    },
+    onError: () => {
+      showNotification({
+        id: "update-todolist",
+        title: "Error",
+        message: "Could not login",
+        color: "red",
+        icon: <IconX />,
+      });
+    },
+  });
+
+  function handleCancelClick(): void {
+    form.setValues({ email: "", password: "" });
+    navigate("/");
+  }
+
+  return (
+    <>
+      <Container>
+        <Title>Login</Title>
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <form onSubmit={form.onSubmit((values) => mutation.mutate(values))}>
+            <Stack>
+              <TextInput
+                label="Email"
+                placeholder="jane@example.com"
+                required
+                {...form.getInputProps("email")}
+              />
+              <PasswordInput
+                label="Password"
+                placeholder="Your strong password"
+                required
+                {...form.getInputProps("password")}
+              />
+              <Group>
+                <Button type="submit">Login</Button>
+                <Button
+                  variant="subtle"
+                  onClick={() => handleCancelClick()}
+                  type="button"
+                >
+                  cancel
+                </Button>
+              </Group>
+            </Stack>
+          </form>
+        </Paper>
+      </Container>
+    </>
+  );
+}
+
+export default LoginPage;
