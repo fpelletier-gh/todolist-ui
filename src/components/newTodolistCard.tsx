7c15a6622b5e1dbcf66f244803325ab953c1ace5
Francis Pelletier <francispelletier2@hotmail.com>
2023-02-09 19:59:54 -0500

    Feat: Add new todolist modal when newTodolistCard is clicked

--- src/components/newTodolistCard.tsx
+++ src/components/newTodolistCard.tsx
@@ -3,12 +3,94 @@ import {
   Card,
   Anchor,
   Title,
+  Button,
+  Group,
+  Stack,
+  Textarea,
+  TextInput,
 } from "@mantine/core";
+import { useFocusTrap } from "@mantine/hooks";
+import { closeAllModals, openModal } from "@mantine/modals";
+import { showNotification } from "@mantine/notifications";
+import { IconX } from "@tabler/icons";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useNewTodolist } from "../hooks";
+import { TodolistPayloadSchema } from "../types";
 
 export default function NewTodolistCard() {
+  const navigate = useNavigate();
+  const { newTodolist } = useNewTodolist();
+  const focusTrapRef = useFocusTrap();
+
+  function NewTodolistForm() {
+    const { register, handleSubmit, reset } = useForm({
+      defaultValues: {
+        title: "",
+        description: "",
+      },
+    });
+
+    function onSubmit(payload: TodolistPayloadSchema) {
+      newTodolist(payload, {
+        onSuccess: (data) => {
+          () => reset();
+          navigate(`/todolist/${data.todolistId}`);
+        },
+        onError: () => {
+          showNotification({
+            id: "new-todolist",
+            title: "Error",
+            message: "Todolist has not been created",
+            color: "red",
+            icon: <IconX />,
+          });
+        },
+      });
+      closeAllModals();
+    }
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)} ref={focusTrapRef}>
+        <Stack>
+          <TextInput
+            label="Title"
+            placeholder="Title"
+            data-autofocus
+            {...register("title")}
+          />
+          <Textarea
+            label="Description"
+            placeholder="Description"
+            autosize
+            {...register("description")}
+          />
+          <Group>
+            <Button type="submit">Save</Button>
+            <Button
+              variant="subtle"
+              onClick={() => closeAllModals()}
+              type="button"
+            >
+              cancel
+            </Button>
+          </Group>
+        </Stack>
+      </form>
+    );
+  }
+
+  function handleNewTodolistClick() {
+    openModal({
+      title: "New todolist",
+      children: <NewTodolistForm />,
+    });
+  }
+
   return (
     <Anchor
       component="button"
+      onClick={handleNewTodolistClick}
       td="none"
       underline={false}
     >
