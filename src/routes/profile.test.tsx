75218cf01baef023de583d363ac596a31f2a9005
Francis Pelletier <francispelletier2@hotmail.com>
2023-02-09 20:49:37 -0500

    fix: add missing profile component

--- src/routes/profile.test.tsx
+++ src/routes/profile.test.tsx
@@ -6,6 +6,7 @@ import { user } from "../mocks/utils";
 describe("Given the user is logged in", () => {
   it("should show the username and email", async () => {
     renderWithClient(<Profile />);
+
     const username = user.username[0].toUpperCase() + user.username.slice(1);
     const email = user.email;
 
