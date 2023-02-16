a8bbb35522a39ed5d6fc1f9e8d79c102f746a516
Francis Pelletier <francispelletier2@hotmail.com>
2023-02-16 15:28:31 -0500

    feat: preserve location after page reload

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Container, Flex } from "@mantine/core";
+import { useEffect } from "react";
 
 function Root() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    // @ts-ignore
+    if (performance.getEntriesByType("navigation")[0].type === "reload") {
+      const lastLocation = localStorage.getItem("last_location");
+      if (lastLocation) {
+        const lastLocationPathname = JSON.parse(lastLocation).pathname;
+        navigate(lastLocationPathname);
+      }
+    }
+    window.addEventListener("beforeunload", function () {
+      localStorage.setItem("last_location", JSON.stringify(location));
+    });
+  }, []);
+
   return (
     <main className="App">
       <Container>
