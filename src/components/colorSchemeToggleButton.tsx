c62525f6e32f1163cff369e9e552bb4138f370dd
Francis Pelletier <francispelletier2@hotmail.com>
2023-04-17 21:14:07 -0400

    fix: a bug with react query when toggling colorScheme

--- src/components/colorSchemeToggleButton.tsx
+++ src/components/colorSchemeToggleButton.tsx
@@ -5,11 +5,17 @@ export default function colorSchemeToggleButton() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
 
+  // TODO: fix the bug with react query when switching color scheme without page reload
+  const handleToggleButtonClick = () => {
+    toggleColorScheme();
+    window.location.reload();
+  };
+
   return (
     <ActionIcon
       variant="transparent"
       color={dark ? "blue.2" : "blue"}
-      onClick={() => toggleColorScheme()}
+      onClick={handleToggleButtonClick}
       title="Toggle color scheme"
     >
       {dark ? <IconSun size={18} /> : <IconMoonStars size={18} />}
