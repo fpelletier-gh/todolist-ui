04a8a649d613f48c88290c62664000bc5ed3324c
Francis Pelletier <francispelletier2@hotmail.com>
2023-04-18 19:46:45 -0400

    refactor: todolists with filter for max card number

--- src/routes/todolists.tsx
+++ src/routes/todolists.tsx
@@ -1,10 +1,17 @@
-import { SimpleGrid, Flex, Title } from "@mantine/core";
+import { SimpleGrid, Flex, Title, Anchor, Box, Container } from "@mantine/core";
+import { Link } from "react-router-dom";
 import NewTodolistCard from "../components/newTodolistCard";
 import TodolistCard from "../components/todolistCard";
 import { useTodolists } from "../hooks";
 import { TodolistSchema } from "../types";
 
-export default function Todolists() {
+export default function Todolists({
+  maxCardNumber,
+  titleLink = false,
+}: {
+  maxCardNumber?: number;
+  titleLink?: boolean;
+}) {
   const todolistsQuery = useTodolists();
 
   if (todolistsQuery.error instanceof Error) {
@@ -13,27 +20,49 @@ export default function Todolists() {
 
   return (
     <>
-      <Flex direction="column" p="md" w="100%">
-        <Title order={2} p="md">
-          Todolists
-        </Title>
+      <Container px="0">
+        {titleLink && (
+          <Title order={2} p="md">
+            <Anchor
+              component={Link}
+              sx={(theme) => ({ "&:hover": { color: theme.colors.blue[4] } })}
+              variant="text"
+              to="/home/todolists"
+            >
+              Todolists
+            </Anchor>
+          </Title>
+        )}
+        {!titleLink && (
+          <Title order={2} p="md">
+            Todolists
+          </Title>
+        )}
         <SimpleGrid
           p="md"
           spacing="xl"
           breakpoints={[
             { maxWidth: 980, cols: 3, spacing: "md" },
             { maxWidth: 755, cols: 2, spacing: "sm" },
-            { maxWidth: 600, cols: 1, spacing: "sm" },
           ]}
           cols={4}
         >
           <NewTodolistCard />
           {todolistsQuery.data &&
-            todolistsQuery.data.map((todolist: TodolistSchema) => (
-              <TodolistCard key={todolist.todolistId} todolist={todolist} />
-            ))}
+            todolistsQuery.data
+              .slice(0, maxCardNumber)
+              .map((todolist: TodolistSchema) => (
+                <TodolistCard key={todolist.todolistId} todolist={todolist} />
+              ))}
         </SimpleGrid>
-      </Flex>
+        {!titleLink && (
+          <Box p="md">
+            <Anchor component={Link} underline={false} to="/home/all">
+              Back to home
+            </Anchor>
+          </Box>
+        )}
+      </Container>
     </>
   );
 }
