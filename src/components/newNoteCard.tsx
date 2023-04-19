f80207367fd38b51fe0bc0d6d8c33e5e42b35995
Francis Pelletier <francispelletier2@hotmail.com>
2023-04-19 16:04:40 -0400

    feat: add newNoteCard component

---
+++ src/components/newNoteCard.tsx
@@ -0,0 +1,72 @@
+import { Text, Card, Anchor, Title, ThemeIcon } from "@mantine/core";
+import { IconPencilPlus } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
+import { useNewNote } from "../hooks";
+import { openNewNoteModal } from "./newNoteForm";
+
+export default function NewNoteCard() {
+  const navigate = useNavigate();
+  const { newNote } = useNewNote();
+
+  function handleNewNoteClick() {
+    openNewNoteModal(navigate, newNote);
+  }
+
+  return (
+    <Anchor
+      component="button"
+      onClick={handleNewNoteClick}
+      td="none"
+      underline={false}
+    >
+      <Card
+        shadow="sm"
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: theme.spacing.md,
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[7]
+              : theme.colors.gray[2],
+          "&:hover": {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[3],
+            borderColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.blue[6]
+                : theme.colors.blue[3],
+          },
+        })}
+        maw="400px"
+        mih="160px"
+        mah="160px"
+        p="sm"
+        radius="md"
+        withBorder
+      >
+        <Card.Section withBorder inheritPadding py="xs">
+          <ThemeIcon
+            sx={(theme) => ({
+              backgroundColor: "transparent",
+
+              color:
+                theme.colorScheme === "dark"
+                  ? theme.colors.white
+                  : theme.colors.dark[8],
+            })}
+          >
+            <IconPencilPlus />
+          </ThemeIcon>
+          <Text size="sm" color="dimmed">
+            Add a new note
+          </Text>
+        </Card.Section>
+      </Card>
+    </Anchor>
+  );
+}
