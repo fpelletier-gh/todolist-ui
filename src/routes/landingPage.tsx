3cd4700a667ff79abfb9224d7f92f7328a8cc19d
Francis Pelletier <francispelletier2@hotmail.com>
2023-04-19 15:51:09 -0400

    chore: delete comments

--- src/routes/landingPage.tsx
+++ src/routes/landingPage.tsx
@@ -125,7 +125,6 @@ const useStyles = createStyles((theme) => ({
 }));
 
 export default function LandingPage() {
-  // TODO: create a nice landing page
   const { user, logout } = useUser();
   const { classes } = useStyles();
 
@@ -209,40 +208,4 @@ export default function LandingPage() {
       </div>
     </Container>
   );
-
-  /* return ( */
-  /*   <Flex w="100%" direction="column" align="center" justify="center" pt="xl"> */
-  /*     <Group position="right" w="100%"> */
-  /*       <Anchor component={Link} variant="text" to="/"> */
-  /*         Home */
-  /*       </Anchor> */
-  /*       <Anchor component={Link} variant="text" to="/login"> */
-  /*         Login */
-  /*       </Anchor> */
-  /*       <Anchor component={Link} variant="text" to="/register"> */
-  /*         Register */
-  /*       </Anchor> */
-  /*     </Group> */
-  /*     <Title order={1} mt="xl"> */
-  /*       Todolists */
-  /*     </Title> */
-  /*     <Text my="lg">A Todolists application to serve all your todo needs</Text> */
-  /**/
-  /*     {!user && ( */
-  /*       <Flex gap="md"> */
-  /*         <Button w="90px" variant="filled" component={Link} to="/register"> */
-  /*           Register */
-  /*         </Button> */
-  /*         <Button w="90px" variant="outline" component={Link} to="/login"> */
-  /*           Login */
-  /*         </Button> */
-  /*       </Flex> */
-  /*     )} */
-  /*     {user && ( */
-  /*       <Button component={Link} to="/todolist"> */
-  /*         Todolists */
-  /*       </Button> */
-  /*     )} */
-  /*   </Flex> */
-  /* ); */
 }
