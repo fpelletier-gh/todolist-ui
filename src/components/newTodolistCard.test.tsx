a3e6a9860f222d7f846623f8f3d310e81ecd46bc
Francis Pelletier <francispelletier2@hotmail.com>
2023-05-01 20:51:25 -0400

    refactor: newTodolistCard tests

--- src/components/newTodolistCard.test.tsx
+++ src/components/newTodolistCard.test.tsx
@@ -1,14 +1,26 @@
 import { screen } from "@testing-library/dom";
 import { renderWithClient } from "../tests/utils/test-utils";
 import NewTodolistCard from "./newTodolistCard";
+import userEvent from "@testing-library/user-event";
+import { within } from "@testing-library/react";
 
 describe("Test NewTodolistCard", () => {
   it("should show a title and a description", async () => {
     renderWithClient(<NewTodolistCard />);
 
-    expect(
-      await screen.findByRole("heading", { name: /New/i })
-    ).toBeInTheDocument();
     expect(await screen.findByText(/Add a new todolist/i)).toBeInTheDocument();
+
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: /add a new todolist/i,
+      })
+    );
+    const dialog = screen.getByRole("dialog", {
+      name: /new todolist/i,
+    });
+
+    expect(within(dialog).getByText(/new todolist/i)).toBeInTheDocument();
+    expect(screen.getByText(/title/i)).toBeInTheDocument();
+    expect(screen.getByText(/description/i)).toBeInTheDocument();
   });
 });
